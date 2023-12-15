644de80847b022f029e721d8fdca46e6f7d6c6c2
Cailloux22 <98032839+Cailloux22@users.noreply.github.com>
2023-12-15 13:51:31 +0100

    aiabfiuzebuigz

--- pdo.php
+++ pdo.php
@@ -1,1 +1,1 @@
-<?php $pdo = new PDO ('mysql:dbname=ter_db;host=terraform-20231214143646949400000001.cmaadob3csml.eu-north-1.rds.amazonaws.com:3306','user','password',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+<?php $pdo = new PDO ('mysql:dbname=ter_db;host=terraform-20231214143646949400000001.cmaadob3csml.eu-north-1.rds.amazonaws.com;port=3306','user','password',array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
